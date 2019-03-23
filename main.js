d4582bda1dca7b1e3715e9887543be8b9636df79
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2019-03-23 17:40:57 +0100

    Polish chart

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-
+const SerialPort = require('serialport');
 
 
 
@@ -7,11 +7,19 @@ electron.app.on('ready', () => {
   let win = new electron.BrowserWindow({width: 800, height: 600});
   //win.setMenu(null);
   win.loadFile('index.html');
-
   
   win.webContents.on('did-finish-load', () => {
-    let data = new Array(30).fill({x: Date.now(), y: 0});
-    let labels = new Array(30).fill( Date.now());  
+    let data = new Array(30).fill({x: Date.now() - 10000, y: 0});
+    let labels = new Array(30).fill( Date.now() - 10000);  
+
+    // BEGIN SERIAL PORT STUFF
+    let uart = new SerialPort('COM3', {
+      baudRate: 38400,
+    })
+    uart.on('data', (arg) => {
+      console.log(arg);
+    })
+    // END SERIAL PORT STUFF
 
     setInterval(() => {
       data.shift();
