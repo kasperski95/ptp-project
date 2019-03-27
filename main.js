5fc0191a9bb817deb5d0b246933da006126c909d
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2019-03-27 13:54:23 +0100

    Refactor

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ const electron = require('electron');
 const SerialPort = require('serialport');
 const ByteLength = require('@serialport/parser-byte-length')
 
-let intervalHandler;
+
+
+// CONFIG FROM ARGS
+let updateIntervalHandler;
+let connect;
+let connection;
 
 electron.app.on('ready', () => {
-  let win = new electron.BrowserWindow({width: 800, height: 600});
+  const win = new electron.BrowserWindow({width: 800, height: 600});
   win.setMenu(null);
   win.loadFile('index.html');
-  
   win.webContents.on('did-finish-load', () => {
     const config = {
       samplesPerSecond: 100,
@@ -16,59 +20,68 @@ electron.app.on('ready', () => {
       samplesAhead: 3,
       packSize: 5
     }
-    let startTime = Date.now();
-    let data = new Array(config.chartSpanInMs / 1000 * config.samplesPerSecond + config.samplesAhead).fill({x: Date.now() - startTime - 10000, y: -1});
-    let labels = new Array(config.chartSpanInMs / 1000 * config.samplesPerSecond + config.samplesAhead).fill( Date.now() - startTime - 10000);  
 
-    // BEGIN SERIAL PORT STUFF
-    let uart = new SerialPort('COM3', {
-      baudRate: 38400,
-    })
-    const parser = uart.pipe(new ByteLength({length: 2}))
-    let packCoutner = 0;
-    let packSum;
-    parser.on('data', (arg) => {
-      let result = arg[1] + arg[0] * 256;
+    // init
+    const startTime = Date.now();
+    let data = new Array(config.chartSpanInMs / 1000 * config.samplesPerSecond + config.samplesAhead).fill({x: Date.now() - startTime - config.chartSpanInMs, y: -1});
 
-      // convert to distance
-      let distance = convertInputToDistance(result);
+    // serialport stuff
+    connect = (connectionConfig) => {
+      if (!connectionConfig.portName || !connectionConfig.baudRate) return;
 
-
-      packSum += distance;
-      packCoutner = (packCoutner + 1) % config.packSize;
-
-      // pack to make chart smoother
-      if (packCoutner == 0) {
-        packSum /= config.packSize;
-        data.shift();
-        data.push({x: Date.now() - startTime, y: packSum});
-        packSum = 0;
-  
-      }
+      let uart = new SerialPort(connectionConfig.portName, {baudRate: parseInt(connectionConfig.baudRate)}, () => {})
       
+      const parser = uart.pipe(new ByteLength({length: 2}));
+      let packCounter = 0;
+      let packSum;
+      parser.on('data', (arg) => {
+        packSum += convertInputToDistance(arg[0] * 256 + arg[1]);
+        packCounter = (packCounter + 1) % config.packSize;
 
-    })
-    // END SERIAL PORT STUFF
+        // pack values to make chart smoother
+        if (packCounter == 0) {
+          packSum /= config.packSize;
+          data.shift();
+          data.push({x: Date.now() - startTime, y: packSum});
+          packSum = 0;
+        }
+      })
 
-    intervalHandler = setInterval(() => {
+      return uart;
+    }
+
+
+    // update 60 times per second
+    updateIntervalHandler = setInterval(() => {
       let y = data[data.length-1].y;
       data[data.length-1] = {x: Date.now() - startTime, y};
-      win.webContents.send('update', {labels, data, config, startTime});
-    }, 16)
+      win.webContents.send('update', {data, config, startTime});
+    }, 1000/60);
   })
 })
 
 
+
+// on close
 electron.app.on('window-all-closed', () => {
-  clearInterval(intervalHandler);
+  clearInterval(updateIntervalHandler);
   electron.app.quit();
 })
 
 
+electron.ipcMain.on('update-config', (e, config) => {
+  if (connection)
+    connection.close(() => {connection = connect(config);})
+  else
+    connection = connect(config);  
+})
+
+
+//----------------------------------------------------------------------------------
+// UTILS
 
 function convertInputToDistance(x) {
   let voltage = x * (3.3 / 4096);
-  //let distance = -5.40274 * Math.pow(voltage,3) + 28.4823 * Math.pow(voltage,2) - 49.7115 * voltage + 31.3444;
   let distance = -2.99702 * Math.pow(voltage,3) + 20.0823 * Math.pow(voltage,2) - 45.7232 * voltage + 40.3485;
   return Math.min(Math.max(distance, 3), 30);
-}
+}  
